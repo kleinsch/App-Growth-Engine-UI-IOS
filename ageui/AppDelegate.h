f49575b1a2a2fb8a160ab656a5d13e286b400be4
Kirk Tsai <tsaik@yahoo.com>
2012-10-11 14:27:28 -0400

    Initial Commit

---
+++ ageui/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ageui
+//
+//  Created by Kirk Tsai on 10/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
